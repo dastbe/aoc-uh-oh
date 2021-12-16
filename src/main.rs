b84668128fb2a2cff0b13e1b6ccfb9593440a4ab
David Bell <david@dbell.dev>
2021-12-16 10:29:17 -0800

    cutting out anything except operator related code

--- src/main.rs
+++ src/main.rs
@@ -2,124 +2,7 @@ use std::vec::Vec;
 use std::iter::Peekable;
 
 pub fn main() {
-    let input = include_str!("input.txt");
-    let packet = decode(convert(input));
-
-    println!("problem 16 part 1: {}", calc_version_sum(&packet));
-    println!("problem 16 part 2: {}", calc_result(&packet));
-}
-
-pub fn calc_version_sum(p: &Packet) -> u64 {
-    return match p {
-        Packet::Literal(l)  => l.version,
-        Packet::Operator(o) => {
-            let mut sum = o.version;
-            for op in &o.packets {
-                sum += calc_version_sum(op);
-            }
-            sum
-        },
-    }
-}
-
-pub fn calc_result(p: &Packet) -> u64 {
-    return match p {
-        Packet::Literal(l)  => l.value,
-        Packet::Operator(o) => { 
-            match o.type_id {
-                // sum
-                0 => {
-                    let mut sum = 0;
-                    for op in &o.packets {
-                        sum += calc_result(op);
-                    }
-                    sum
-                },
-                // product
-                1 => {
-                    let mut product = 1;
-                    for op in &o.packets {
-                        product *= calc_result(op);
-                    }
-                    product
-                },
-                // min
-                2 => {
-                    let mut minimum = u64::MAX;
-                    for op in &o.packets {
-                        minimum = std::cmp::min(calc_result(op), minimum);
-                    }
-                    minimum
-                },
-                // max
-                3 => {
-                    let mut maximum = u64::MIN;
-                    for op in &o.packets {
-                        maximum = std::cmp::max(calc_result(op), maximum);
-                    }
-                    maximum
-                },
-                // gt
-                5 => {
-                    let first = calc_result(o.packets.get(0).expect("has first"));
-                    let second = calc_result(o.packets.get(1).expect("has second"));
-
-                    match first > second {
-                        true => 1,
-                        false => 0,
-                    }
-                },
-                // lt
-                6 => {
-                    let first = calc_result(o.packets.get(0).expect("has first"));
-                    let second = calc_result(o.packets.get(1).expect("has second"));
-
-                    match first < second {
-                        true => 1,
-                        false => 0,
-                    }
-                },
-                // eq
-                7 => {
-                    let first = calc_result(o.packets.get(0).expect("has first"));
-                    let second = calc_result(o.packets.get(1).expect("has second"));
-
-                    match first == second {
-                        true => 1,
-                        false => 0,
-                    }
-                },
-                _ => panic!("unknown type")
-            }
-        },
-    }
-}
-
-pub fn convert(hex: &str) -> Vec<u64> {
-    let mut res = Vec::new();
-    for c in hex.trim().chars() {
-        match c {
-            '0' => { res.append(&mut vec![0,0,0,0]) },
-            '1' => { res.append(&mut vec![0,0,0,1]) },
-            '2' => { res.append(&mut vec![0,0,1,0]) },
-            '3' => { res.append(&mut vec![0,0,1,1]) },
-            '4' => { res.append(&mut vec![0,1,0,0]) },
-            '5' => { res.append(&mut vec![0,1,0,1]) },
-            '6' => { res.append(&mut vec![0,1,1,0]) },
-            '7' => { res.append(&mut vec![0,1,1,1]) },
-            '8' => { res.append(&mut vec![1,0,0,0]) },
-            '9' => { res.append(&mut vec![1,0,0,1]) },
-            'A' => { res.append(&mut vec![1,0,1,0]) },
-            'B' => { res.append(&mut vec![1,0,1,1]) },
-            'C' => { res.append(&mut vec![1,1,0,0]) },
-            'D' => { res.append(&mut vec![1,1,0,1]) },
-            'E' => { res.append(&mut vec![1,1,1,0]) },
-            'F' => { res.append(&mut vec![1,1,1,1]) },
-             _  => { panic!("Unkown hex={}", c) },
-        }
-    }
-
-    return res;
+    let packet = decode(vec![1,0,1,0]);
 }
 
 pub fn decode(bitstream: Vec<u64>) -> Packet {
@@ -130,11 +13,14 @@ pub fn decode(bitstream: Vec<u64>) -> Packet {
     }
 }
 
+//pub fn decode_packets(bitstream: Vec<u64>) -> Vec<Packet> {
 pub fn decode_packets<I: Iterator<Item = u64>>(itr: &mut Peekable<I>) -> Vec<Packet> {
+    //let mut bitstream_itr = bitstream.into_iter().peekable();
 
     let mut res = Vec::new();
     loop {
         match decode_packet(itr) {
+        //match decode_packet(&mut bitstream_itr) {
             Some(p) => { res.push(p); },
             None    => break
         }
@@ -149,30 +35,8 @@ pub fn decode_packet<I: Iterator<Item = u64>>(itr: &mut Peekable<I>) -> Option<P
     let type_id = extend_number(0, itr, 3)?;
 
     return match type_id {
-        4 => literal_packet(version, type_id, itr),
         _ => operator_packet(version, type_id, itr),
     };
-}
-
-pub fn literal_packet<I: Iterator<Item = u64>>(version: u64, type_id: u64, itr: &mut Peekable<I>) -> Option<Packet> {
-    let mut literal = 0;
-    // process 5-tuples that start with 1
-    while itr.peek() == Some(&1) {
-        itr.next()?;
-        literal = extend_number(literal, itr, 4)?;
-    }
-
-    // process last tuple
-    itr.next()?;
-    literal = extend_number(literal, itr, 4)?;
-
-    let p = LiteralPacket {
-        version: version,
-        type_id: type_id,
-        value: literal,
-    };
-
-    return Some(Packet::Literal(p));
 }
 
 pub fn operator_packet<I: Iterator<Item = u64>>(version: u64, type_id: u64, itr: &mut Peekable<I>) -> Option<Packet> {
@@ -180,10 +44,16 @@ pub fn operator_packet<I: Iterator<Item = u64>>(version: u64, type_id: u64, itr:
     if length_type_id == 0 {
         let length = extend_number(0, itr, 15)?;
 
+        /*let mut sub_bits = Vec::new();
+        for _ in 0..length {
+            sub_bits.push(itr.next()?);
+        }*/
+
         let p = OperatorPacket {
             version: version,
             type_id: type_id,
             packets: decode_packets(&mut itr.take(length as usize).peekable()),
+            //packets: decode_packets(sub_bits),
         };
 
         return Some(Packet::Operator(p));
@@ -216,15 +86,7 @@ pub fn extend_number<I: Iterator<Item = u64>>(num: u64, itr: &mut Peekable<I>, t
 
 #[derive(Debug)]
 pub enum Packet {
-    Literal(LiteralPacket),
     Operator(OperatorPacket),
-}
-
-#[derive(Debug)]
-pub struct LiteralPacket {
-    version: u64,
-    type_id: u64,
-    value: u64,
 }
 
 #[derive(Debug)]
