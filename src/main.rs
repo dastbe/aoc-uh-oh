297ba519d75e1d30dea6c601db3e98350153054a
David Bell <david@dbell.dev>
2021-12-16 10:31:44 -0800

    before: panics

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,10 @@ pub fn decode(bitstream: Vec<u64>) -> Packet {
     }
 }
 
-//pub fn decode_packets(bitstream: Vec<u64>) -> Vec<Packet> {
 pub fn decode_packets<I: Iterator<Item = u64>>(itr: &mut Peekable<I>) -> Vec<Packet> {
-    //let mut bitstream_itr = bitstream.into_iter().peekable();
-
     let mut res = Vec::new();
     loop {
         match decode_packet(itr) {
-        //match decode_packet(&mut bitstream_itr) {
             Some(p) => { res.push(p); },
             None    => break
         }
@@ -33,42 +29,14 @@ pub fn decode_packet<I: Iterator<Item = u64>>(itr: &mut Peekable<I>) -> Option<P
     // get version digits
     let version = extend_number(0, itr, 3)?;
     let type_id = extend_number(0, itr, 3)?;
-
-    return match type_id {
-        _ => operator_packet(version, type_id, itr),
-    };
+    return operator_packet(version, type_id, itr);
 }
 
 pub fn operator_packet<I: Iterator<Item = u64>>(version: u64, type_id: u64, itr: &mut Peekable<I>) -> Option<Packet> {
-    let length_type_id = extend_number(0, itr, 1)?;
-    if length_type_id == 0 {
-        let length = extend_number(0, itr, 15)?;
-
-        /*let mut sub_bits = Vec::new();
-        for _ in 0..length {
-            sub_bits.push(itr.next()?);
-        }*/
-
-        let p = OperatorPacket {
-            version: version,
-            type_id: type_id,
-            packets: decode_packets(&mut itr.take(length as usize).peekable()),
-            //packets: decode_packets(sub_bits),
-        };
-
-        return Some(Packet::Operator(p));
-    }
-
-    let num_packets = extend_number(0, itr, 11)?;
-    let mut packets = Vec::new();
-    for _ in 0..num_packets {
-        packets.push(decode_packet(itr)?);
-    }
-
     let p = OperatorPacket {
         version: version,
         type_id: type_id,
-        packets: packets,
+        packets: decode_packets(&mut itr.take(0).peekable()),
     };
 
     return Some(Packet::Operator(p));
